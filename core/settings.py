87b3a3dad65fb3fe27eec78182a0a564d2519ec2
jaychovatiya4995 <jaychovatiya4995@gmail.com>
2021-08-09 16:38:12 +0530

    allow hosts with ip

--- core/settings.py
+++ core/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['178.128.144.39']
 # Application definition
 
 INSTALLED_APPS = [
